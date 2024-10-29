2c2d62bc55eec8ecd608a0849ce344913398ec95
Abelsabater <abesabmun@alu.edu.gva.es>
2024-10-29 14:08:38 +0100

    Tema 3

--- src/main/ArrayssII/SumaIgual.java
+++ src/main/ArrayssII/SumaIgual.java
@@ -18,7 +18,17 @@ public class SumaIgual {
 
     public static void main(String[] args) {
         int numeros;
+        int num2;
         Scanner sc= new Scanner(System.in);
-        System.out.println("Dime cuants numeros vols per a la");
+        System.out.println("Dime cuants numeros vols per a la llista");
+        numeros= sc.nextInt();
+        int llista[]=new int[numeros];
+        for (int i = 0; i < numeros; i++) {
+            System.out.println("Dime un numero");
+            llista[i] = sc.nextInt();
+        }
+        System.out.println("Dime quin numero vols que trague");
+        num2=sc.nextInt();
+        System.out.println(suma(llista,num2));
     }
 }
