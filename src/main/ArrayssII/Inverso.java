2f141b210c3df04b1cf947cf723296e8132f2faf
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-05 14:41:12 +0100

    Tema 3

--- src/main/ArrayssII/Inverso.java
+++ src/main/ArrayssII/Inverso.java
@@ -1,7 +1,19 @@
 package ArrayssII;
 
+import java.util.Arrays;
+
 public class Inverso {
+    public static int [] inverso(int [] llista){
+        int [] llista2=new int [llista.length];
+        for (int i = 0; i < llista.length; i++) {
+            llista2[i]=llista[llista.length-1-i];
+        }
+        return llista2;
+    }
+
     public static void main(String[] args) {
+        int[] llista={1,2,3,4,4};
+        System.out.println(Arrays.toString(inverso(llista)));
 
     }
 }
